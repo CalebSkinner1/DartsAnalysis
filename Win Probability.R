10e945374f4e8674b442c180ad37a149cd5395a7
Caleb Skinner <calebskinner01@gmail.com>
2025-07-31 10:04:05 -0500

    add game_probability function

--- Win Probability.R
+++ Win Probability.R
@@ -12,38 +12,54 @@ data <- read_excel("DartsAnalysis/Maxfield - Darts.xlsx") %>%
   filter(!is.na(date)) %>%
   filter(base != "bad magnet", angle == "vertical")
 
-compute_player_probabilities <- function(player, data, height){
-  player <- str_to_lower(player)
+compute_player_probabilities <- function(players, data, height){
+  probs_table <- map(players, ~{
+    player <- str_to_lower(.x)
+    
+    past_scores <- data %>% filter(height == height) %>% pull(any_of(player)) %>% na.omit() %>% as.numeric()
+    
+    samples <- mh_sampler(past_scores)
+    
+    samples$new_y %>% table() %>% as_tibble() %>%
+      rename("score" = ".") %>%
+      mutate(perc = n/length(samples$new_y)) %>%
+      select(score, perc)
+  })
+  names(probs_table) <- players
   
-  past_scores <- data %>% filter(height == height) %>% pull(any_of(player)) %>% na.omit() %>% as.numeric()
-  
-  samples <- mh_sampler(past_scores)
-  
-  samples$new_y %>% table() %>% as_tibble() %>%
-    rename("score" = ".") %>%
-    mutate(perc = n/length(samples$new_y)) %>%
-    select(score, perc)
+  probs_table
 }
 
-sum_mult_rounds <- function(probs_table, n) {
-  reduce(
-    rep(list(probs_table), n),
-    function(a, b) {
-      merge(a, b, by = NULL) %>%
-        transmute(
-          score = as.numeric(score.x) + as.numeric(score.y),
-          perc = perc.x * perc.y
-        ) %>%
-        group_by(score) %>%
-        summarise(perc = sum(perc), .groups = "drop")
-    }
-  )
+# sum multiple rounds together (r is number of rounds)
+sum_mult_rounds <- function(probs_table, r) {
+  if(r == 0){
+    tibble(score = 0, perc = 1)
+  }else{
+    reduce(
+      rep(list(probs_table), r),
+      function(a, b) {
+        merge(a, b, by = NULL) %>%
+          transmute(
+            score = as.numeric(score.x) + as.numeric(score.y),
+            perc = perc.x * perc.y
+          ) %>%
+          group_by(score) %>%
+          summarise(perc = sum(perc), .groups = "drop")
+      }
+    )
+  }
 }
 
+# find win rates from distributions
 compare_distributions <- function(probs_table, names = NULL) {
   # dist_list: list of tibbles with columns outcome & prob
   n <- length(probs_table)
-  if (is.null(names)) names <- paste0("player", seq_len(n))
+  if (is.null(names)){
+    if(is.null(names(probs_table))){
+      names <- paste0("player", seq_len(n))
+    }else{
+      names <- names(probs_table)
+    }}
   
   # 1. Build full grid of outcomes
   grids <- probs_table %>% 
@@ -63,46 +79,50 @@ compare_distributions <- function(probs_table, names = NULL) {
     mutate(across(contains("score"), ~if_else(winners > 1 & .x == "win", "tie", .x))) %>%
     select(contains("score"), perc)
   
+  # straight up win
   win_table <- map_dfr(names, ~{
     lookup <- paste0(.x, "_score")
     
-    full %>% filter(!!sym(lookup) == "win") %>% summarize(win = sum(perc)) %>% mutate(player = .x)})
+    full %>% filter(!!sym(lookup) == "win") %>% summarize(result = sum(perc)) %>% mutate(player = .x, type = "win")})
   
   
-  ties_names <- map(seq_along(2:n), ~combn(names, .x, simplify = FALSE)) %>% flatten()
+  ties_names <- map(c(2:n), ~combn(names, .x, simplify = FALSE)) %>% flatten()
   
   tie_table <- map_dfr(ties_names, ~{
     lookup <- paste0(.x, "_score")
+    temp_ties_df <- full
     
-    # HERE
-    full %>% filter(!!sym(lookup[1]) == "tie", ) %>% summarize(win = sum(perc)) %>% mutate(player = .x)})
+    for(i in seq_along(lookup)){
+      temp_ties_df <- temp_ties_df %>%
+        filter(!!sym(lookup[i]) == "tie")
+    }
+    temp_ties_df %>%
+      rowwise() %>%
+      mutate(ties_num = sum(c_across(contains("score")) == "tie")) %>%
+      filter(ties_num == length(lookup)) %>%
+      ungroup() %>%
+      summarize(result = sum(perc)) %>%
+      mutate(player = str_flatten_comma(.x), type = "overtime")})
   
-  # 3. Compute win/tie
-  full <- full %>%
-    mutate(
-      joint_prob = reduce(map(seq_len(n),
-                              ~pull(full, paste0("p", .x))),
-                          `*`),
-      max_val = pmax(!!!syms(names)),
-      # which ones tie for max?
-      is_max = map(names, ~.[[.x]] == max_val),
-      tie_group = map_chr(is_max, ~ paste(names[.x][.], collapse = "_"))
-    )
-  
-  # 4. Aggregate
-  summary <- full %>%
-    group_by(tie_group) %>%
-    summarise(prob = sum(joint_prob), .groups = "drop") %>%
-    arrange(desc(prob))
-  
-  list(detail = full, summary = summary)
+  # aggregate
+  bind_rows(win_table, tie_table)
 }
 
-# HERE
+# find win rates from overtime
+compute_overtime <- function(probs_table, win_rates){
+  overtimes_table <- win_rates %>% filter(type == "overtime")
+  
+  ties_names <- overtimes_table %>% pull(player)
+  
+  weights <- overtimes_table %>% pull(result)
+  
+  map2_dfr(ties_names, weights, ~probs_table[str_split(.x, ", ")[[1]]] %>%
+        compare_distributions() %>%
+        filter(type == "win") %>%
+        mutate(result = result/sum(result)*.y))
+}
 
-game_tibble <- tibble(round = c(1, 2, 3), caleb = c(0, 0, NA), joshua = c(1, 2, NA))
-
-compute_win_probability <- function(game_tibble, data, height){
+compute_win_probability <- function(game_tibble, data, height, player_probabilities){
   game_table <- game_tibble %>% select(-round)
   
   players <- game_table %>% colnames()
@@ -111,17 +131,109 @@ compute_win_probability <- function(game_tibble, data, height){
   
   current_score <- game_table %>% group_by() %>% summarize(across(everything(), ~sum(.x, na.rm = TRUE)))
   
-  tic()
-  player_probabilities <- map(players, ~compute_player_probabilities(.x, data, height))
-  toc()
+  if(missing(player_probabilities)){
+    tic()
+    player_probabilities <- compute_player_probabilities(players, data, height)
+    names(player_probabilities) <- players
+    toc()
+  }else{
+    player_probabilities <- player_probabilities[players]
+  }
   
+  # convert player probabilities to remaining rounds
   remaining_rounds_table <- pmap(list(player_probabilities, rounds_left, current_score),
                                  function(prob_table, rounds, current_score){
                                    sum_mult_rounds(prob_table, rounds) %>%
                                      mutate(score = as.numeric(score) + current_score)})
   
+  # win rate before overtime
+  win_rates <- compare_distributions(remaining_rounds_table)
 
+  # overtime
+  win_rates %>% filter(type == "win") %>%
+    bind_rows(compute_overtime(player_probabilities, win_rates)) %>%
+    group_by(player) %>%
+    summarize(win_prob = sum(result))
 }
 
+# examples
+# can compute this ahead of time (if computing multiple game probabilities) or in compute_win_probability function
+tic()
+player_probabilities <- c("caleb", "joshua", "quadri", "joseph", "daniel") %>% compute_player_probabilities(data, "mid")
+toc()
 
-compute_player_probabilities(game_tibble, data = data, height = "mid")
+# game_tibble <- tibble(round = c(1, 2, 3), caleb = c(0, 0, NA), joshua = c(1, 2, NA))
+# compute_win_probability(game_tibble, data = data, height = "mid", player_probabilities)
+# 
+# game_tibble <- tibble(round = c(1, 2, 3), caleb = c(NA, NA, NA), joshua = c(NA, NA, NA), quadri = c(1, NA, NA))
+# compute_win_probability(game_tibble, data = data, height = "mid", player_probabilities)
+# 
+# full_game_tibble <- tibble(round = c(1, 2, 3), quadri = c(4, 1, 3), joshua = c(0, 4, 1))
+
+# finds player name from abbreviated letter
+convert_name <- function(letter){
+  case_when(
+    letter == "C" ~ "caleb",
+    letter == "J" ~ "joshua",
+    letter == "Q" ~ "quadri",
+    letter == "B" ~ "joseph",
+    letter == "D" ~ "daniel",
+    letter == "V" ~ "veronica",
+    letter == "K" ~ "kenny",
+    letter == "Y" ~ "qianzi",
+    .default = NA)
+  
+}
+
+# converts order abbreviation into list of players
+convert_order <- function(order){
+  map_chr(seq_len(str_length(order)), ~str_sub(order, start = .x, end = .x) %>% convert_name())
+}
+
+# charts win probability of game
+chart_game_probability <- function(full_game_tibble, order, data, height, player_probabilities){
+  players <- convert_order(order)
+  
+  game_long <- full_game_tibble %>% pivot_longer(cols = all_of(players), values_to = "score", names_to = "player") %>%
+    mutate(player = factor(player, levels = players)) %>%
+    arrange(round, player)
+  
+  win_prob_data <- map_dfr(0:nrow(game_long), ~{
+    game_long %>% mutate(index = row_number(),
+                  score = if_else(index > .x, NA, score)) %>%
+      select(-index) %>%
+      pivot_wider(names_from = "player", values_from = "score") %>%
+      compute_win_probability(data = data, height = height, player_probabilities = player_probabilities) %>%
+      pivot_wider(names_from = "player", values_from = "win_prob") %>%
+      mutate(throw = .x)
+    }) %>%
+    pivot_longer(cols = all_of(players), values_to = "win_prob", names_to = "player")
+  
+  win_prob_data %>%
+    ggplot() +
+    geom_line(aes(x = throw, y = win_prob, color = player)) +
+    labs(x = "Throw", y = "Win Probability")
+}
+
+game_chart_wrapper <- function(game_id, data, player_probabilities){
+  specific_game <- data %>% filter(game == game_id)
+  
+  order <- specific_game %>% slice(1) %>% pull(order)
+  
+  height <- specific_game %>% slice(1) %>% pull(height)
+  
+  game_tibble <- specific_game %>% select(round, all_of(convert_order(order))) %>%
+    mutate(across(everything(), ~as.numeric(.x)))
+  
+  chart_game_probability(full_game_tibble = game_tibble, order = order, data = data, height = height, player_probabilities = player_probabilities) +
+    labs(title = str_c("Game ", game_id, " Win Probability", y = ""))
+}
+
+# chart game 180 Joshua vs Quadri -----------------------------------------------------
+# tibble(round = c(1, 2, 3), quadri = c(4, 1, 3), joshua = c(0, 4, 1)) %>%
+#   chart_game_probability(order = "QJ", data = data, height = "mid", player_probabilities)
+
+tic()
+game_chart_wrapper(126, data, player_probabilities)
+toc()
+
